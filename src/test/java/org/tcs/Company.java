9cf659f50b03c2561135b01a2fb0bd03ecd223a0
prabhuvs1994 <prabhuvs1994@gmail.com>
2022-08-28 12:01:23 +0530

    message

---
+++ src/test/java/org/tcs/Company.java
@@ -0,0 +1,12 @@
+package org.tcs;
+
+public class Company {
+	private void railway() {
+		// TODO Auto-generated method stub
+
+	}
+private void police() {
+	// TODO Auto-generated method stub
+
+}
+}
